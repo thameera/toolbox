56bc09f688985a06608d3b2d260dcd8dd5a9648c
Thameera Senanayaka <me@thameera.com>
2024-06-10 00:47:08 +1000

    URL Parser: support partial URLs

--- src/lib/parsers/url-parser.ts
+++ src/lib/parsers/url-parser.ts
@@ -7,15 +7,22 @@ const parseHash = (hash: string): Record<string, string> => {
 
 export const parseURL = (input: string): IParsedURL | null => {
   try {
+    // Support partial URLs like "/path?query=1"
+    let isPartial = false;
+    if (!input.includes("://") && input.startsWith("/")) {
+      input = `http://example.com${input}`;
+      isPartial = true;
+    }
+
     const url = new URL(input);
     const query = Object.fromEntries(url.searchParams.entries());
     const hash = parseHash(url.hash);
 
     return {
       type: "url",
-      protocol: url.protocol,
-      host: url.host,
-      port: url.port,
+      protocol: isPartial ? "" : url.protocol,
+      host: isPartial ? "" : url.host,
+      port: isPartial ? "" : url.port,
       path: url.pathname,
       username: url.username,
       password: url.password,
