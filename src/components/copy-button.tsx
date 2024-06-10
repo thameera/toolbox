62ae46ece83e729f7f8f2e67d8120b1b2f79cdae
Thameera Senanayaka <me@thameera.com>
2024-06-10 17:10:09 +1000

    Add tooltip to copy button

--- src/components/copy-button.tsx
+++ src/components/copy-button.tsx
@@ -1,5 +1,11 @@
 import { Button } from "@/components/ui/button";
 import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import {
   ClipboardIcon,
   ClipboardDocumentCheckIcon,
 } from "@heroicons/react/24/outline";
@@ -21,16 +27,23 @@ export function CopyButton({ text, className }: CopyButtonProps): JSX.Element {
   };
 
   return (
-    <Button
-      variant="outline"
-      className={cn("p-2", className)}
-      onClick={handleCopy}
-    >
-      {copied ? (
-        <ClipboardDocumentCheckIcon className="w-4 h-4" />
-      ) : (
-        <ClipboardIcon className="w-4 h-4 opacity-50" />
-      )}
-    </Button>
+    <TooltipProvider delayDuration={200}>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Button
+            variant="outline"
+            className={cn("p-2", className)}
+            onClick={handleCopy}
+          >
+            {copied ? (
+              <ClipboardDocumentCheckIcon className="w-4 h-4" />
+            ) : (
+              <ClipboardIcon className="w-4 h-4 opacity-50" />
+            )}
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent>Copy</TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   );
 }
