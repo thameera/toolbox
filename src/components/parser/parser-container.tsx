8fec65d76960de140451e099874930f5172c8b8b
Thameera Senanayaka <me@thameera.com>
2024-06-14 23:16:09 +1000

    Make the second parser column toggle-able

--- src/components/parser/parser-container.tsx
+++ src/components/parser/parser-container.tsx
@@ -1,13 +1,38 @@
+import { useState } from "react";
+import { Button } from "../ui/button";
 import { ParserColumn } from "./parser-column";
+import {
+  ChevronDoubleLeftIcon,
+  ChevronDoubleRightIcon,
+} from "@heroicons/react/24/outline";
 
 export function ParserContainer(): JSX.Element {
+  const [twoColumns, setTwoColumns] = useState(true);
+
+  const toggleColumns = () => {
+    setTwoColumns((prev) => !prev);
+  };
+
   return (
-    <div className="flex flex-row">
-      <div className="w-1/2">
-        <ParserColumn />
+    <div>
+      <div className="pl-4 pr-4 flex flex-row-reverse">
+        <Button variant="outline" className="p-2" onClick={toggleColumns}>
+          {twoColumns ? (
+            <ChevronDoubleRightIcon className="w-4 h-4" />
+          ) : (
+            <ChevronDoubleLeftIcon className="w-4 h-4" />
+          )}
+        </Button>
       </div>
-      <div className="w-1/2">
-        <ParserColumn />
+      <div className="flex flex-row">
+        <div className={twoColumns ? "w-1/2" : "w-screen"}>
+          <ParserColumn />
+        </div>
+        {twoColumns && (
+          <div className="w-1/2">
+            <ParserColumn />
+          </div>
+        )}
       </div>
     </div>
   );
