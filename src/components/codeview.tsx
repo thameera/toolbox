f88b4670ca1d3460c3aff51a90020f412869fcc9
Thameera Senanayaka <me@thameera.com>
2024-06-23 23:09:30 +1000

    Change CodeView theme based on light/dark mode

--- src/components/codeview.tsx
+++ src/components/codeview.tsx
@@ -1,8 +1,12 @@
+"use client";
+
 import CodeMirror from "@uiw/react-codemirror";
+import { githubLight, githubDark } from "@uiw/codemirror-theme-github";
 import { json } from "@codemirror/lang-json";
 import { xml } from "@codemirror/lang-xml";
 import { EditorView } from "@codemirror/view";
 import { CopyButton } from "@/components/copy-button";
+import { useTheme } from "next-themes";
 
 type CodeViewProps = {
   code: string;
@@ -24,6 +28,9 @@ export default function CodeView({
   code,
   language,
 }: CodeViewProps): JSX.Element {
+  const { theme } = useTheme();
+  const cmTheme = theme === "dark" ? githubDark : githubLight;
+
   const extensions = [
     getLanguageExtension(language),
     EditorView.theme({
@@ -40,7 +47,13 @@ export default function CodeView({
       <div className="absolute top-1 right-1 z-10">
         <CopyButton text={code} />
       </div>
-      <CodeMirror value={code} height="auto" extensions={extensions} readOnly />
+      <CodeMirror
+        value={code}
+        theme={cmTheme}
+        height="auto"
+        extensions={extensions}
+        readOnly
+      />
     </div>
   );
 }
