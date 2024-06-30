d6e0ac81f2330a694d4ac11843d4101f6ba2a4da
Thameera Senanayaka <me@thameera.com>
2024-06-30 21:47:32 +1000

    Prevent re-rendering tab content on tab change

--- src/components/tab-container.tsx
+++ src/components/tab-container.tsx
@@ -2,22 +2,51 @@
 
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { ParserContainer } from "./parser/parser-container";
+import { useState } from "react";
 
 export default function TabContainer(): JSX.Element {
+  const [activeTab, setActiveTab] = useState("parser");
+
+  const handleTabChange = (value: string) => {
+    setActiveTab(value);
+  };
+
   return (
-    <Tabs defaultValue="parser">
+    <Tabs defaultValue="parser" onValueChange={handleTabChange}>
       <TabsList>
         <TabsTrigger value="parser">Parser</TabsTrigger>
         <TabsTrigger value="converter">Converter</TabsTrigger>
         <TabsTrigger value="diff">Diff</TabsTrigger>
         <TabsTrigger value="ref">Reference</TabsTrigger>
       </TabsList>
-      <TabsContent value="parser">
+      <TabsContent
+        value="parser"
+        forceMount
+        className={activeTab === "parser" ? "" : "hidden"}
+      >
         <ParserContainer />
       </TabsContent>
-      <TabsContent value="converter">TODO</TabsContent>
-      <TabsContent value="diff">TODO</TabsContent>
-      <TabsContent value="ref">TODO</TabsContent>
+      <TabsContent
+        value="converter"
+        forceMount
+        className={activeTab === "converter" ? "" : "hidden"}
+      >
+        TODO
+      </TabsContent>
+      <TabsContent
+        value="diff"
+        forceMount
+        className={activeTab === "diff" ? "" : "hidden"}
+      >
+        TODO
+      </TabsContent>
+      <TabsContent
+        value="ref"
+        forceMount
+        className={activeTab === "ref" ? "" : "hidden"}
+      >
+        TODO
+      </TabsContent>
     </Tabs>
   );
 }
