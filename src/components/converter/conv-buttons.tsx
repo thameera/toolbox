f5c1072a9a29bbb01d372c75a9674bcae4b346bf
Thameera Senanayaka <me@thameera.com>
2024-07-16 23:02:53 +1000

    Make Converter UI responsive

--- src/components/converter/conv-buttons.tsx
+++ src/components/converter/conv-buttons.tsx
@@ -12,19 +12,19 @@ export function ConverterButtons({
   };
 
   return (
-    <div className="flex flex-col items-center space-x-2">
-      <div className="flex flex-col gap-8">
+    <div className="flex flex-col items-center sm:space-x-2 space-y-4">
+      <div className="flex flex-col gap-8 w-full sm:w-auto">
         {/* Start of button sets */}
         {/* Web Encodings */}
         <div className="flex flex-col gap-2">
           <div className="font-semibold">Web Encodings</div>
-          <div className="flex gap-2">
+          <div className="flex flex-col sm:flex-row gap-2 flex-wrap">
             <ConvButton taskId="base64_encode" label="Base64 Encode" />
             <ConvButton taskId="base64_decode" label="Base64 Decode" />
             <ConvButton taskId="url_encode" label="URL Encode" />
             <ConvButton taskId="url_decode" label="URL Decode" />
           </div>
-          <div className="flex gap-2">
+          <div className="flex flex-col sm:flex-row gap-2 flex-wrap">
             <ConvButton taskId="html_encode" label="HTML Encode" />
             <ConvButton taskId="html_decode" label="HTML Decode" />
           </div>
@@ -33,13 +33,13 @@ export function ConverterButtons({
         {/* Time converters */}
         <div className="flex flex-col gap-2">
           <div className="font-semibold">Time Conversion</div>
-          <div className="flex items-center gap-2">
-            <span>Convert from</span>
+          <div className="flex flex-col sm:flex-row items-center gap-2 flex-wrap">
+            <span className="sm:w-auto w-full text-center">Convert from</span>
             <ConvButton taskId="format_date_from_unix_s" label="Unix seconds" />
             <ConvButton taskId="format_date_from_unix_ms" label="Unix millis" />
           </div>
-          <div className="flex items-center gap-2">
-            <span>Convert to</span>
+          <div className="flex flex-col sm:flex-row items-center gap-2 flex-wrap">
+            <span className="sm:w-auto w-full text-center">Current time</span>
             <ConvButton
               taskId="current_time_formatted_local"
               label="Formatted"
