6d05e80324331415486af7479c52cee1969a0d39
Thameera Senanayaka <me@thameera.com>
2024-07-21 21:09:23 +1000

    Fix numbers being identified as JSON in parser

--- src/lib/parsers/json-paser.ts
+++ src/lib/parsers/json-paser.ts
@@ -4,6 +4,12 @@ import { IParsedJSON } from "./types";
 export function parseJSON(json: string): IParsedJSON | null {
   try {
     const parsed = JSON5.parse(json);
+
+    // If a number is passed, JSON5 will still consider it as valid JSON, so we need this check
+    if (typeof parsed !== "object" || parsed === null) {
+      return null;
+    }
+
     return {
       type: "json",
       json: parsed,
