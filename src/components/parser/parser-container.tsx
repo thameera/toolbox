0c24524a2e9117f682b21cca5547ac235b000aad
Thameera Senanayaka <me@thameera.com>
2024-07-25 23:57:24 +1000

    Support up to 4 columns in parser

--- src/components/parser/parser-container.tsx
+++ src/components/parser/parser-container.tsx
@@ -1,7 +1,7 @@
 import { useRef, useState } from "react";
 import { Button } from "../ui/button";
+import { Toggle } from "@/components/ui/toggle";
 import { ParserColumn, ParserColumnRef } from "./parser-column";
-import { LuChevronsLeft, LuChevronsRight } from "react-icons/lu";
 import {
   Tooltip,
   TooltipContent,
@@ -11,7 +11,12 @@ import {
 import { randomParserExample } from "@/lib/parsers/random-example";
 
 export function ParserContainer(): JSX.Element {
-  const [twoColumns, setTwoColumns] = useState(true);
+  const [columnsVisibility, setColumnsVisibility] = useState([
+    true,
+    true,
+    false,
+    false,
+  ]);
 
   const parserColumnRef = useRef<ParserColumnRef>(null);
 
@@ -20,9 +25,22 @@ export function ParserContainer(): JSX.Element {
     parserColumnRef.current?.setTextInput(example);
   };
 
-  const toggleColumns = () => {
-    setTwoColumns((prev) => !prev);
+  const toggleColumn = (index: number) => {
+    if (index === 0) return; // First column cannot be turned off
+    setColumnsVisibility((prev) => {
+      const newVisibility = [...prev];
+      newVisibility[index] = !newVisibility[index];
+      return newVisibility;
+    });
   };
+
+  const visibleColumnsCount = columnsVisibility.filter(Boolean).length;
+  const columnWidthClass = {
+    1: "w-full",
+    2: "w-1/2",
+    3: "w-1/3",
+    4: "w-1/4",
+  }[visibleColumnsCount];
 
   return (
     <div>
@@ -32,30 +50,37 @@ export function ParserContainer(): JSX.Element {
             Random example
           </Button>
         </div>
-        <TooltipProvider delayDuration={200}>
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <Button variant="outline" className="p-2" onClick={toggleColumns}>
-                {twoColumns ? (
-                  <LuChevronsRight className="w-4 h-4" />
-                ) : (
-                  <LuChevronsLeft className="w-4 h-4" />
-                )}
-              </Button>
-            </TooltipTrigger>
-            <TooltipContent>
-              {twoColumns ? "Hide second column" : "Show second column"}
-            </TooltipContent>
-          </Tooltip>
-        </TooltipProvider>
+        <div className="flex space-x-2">
+          {[1, 2, 3, 4].map((num) => (
+            <Toggle
+              key={num}
+              pressed={columnsVisibility[num - 1]}
+              onPressedChange={() => toggleColumn(num - 1)}
+            >
+              {num}
+            </Toggle>
+          ))}
+        </div>
       </div>
 
       <div className="flex flex-row">
-        <div className={`pr-2 ${twoColumns ? "w-1/2" : "w-full"}`}>
+        <div
+          className={`pr-2 ${columnsVisibility[0] ? columnWidthClass : "w-0 invisible"}`}
+        >
           <ParserColumn ref={parserColumnRef} />
         </div>
         <div
-          className={`transition-width duration-300 pl-2 ${twoColumns ? "w-1/2" : "w-0 hidden"}`}
+          className={`transition-width duration-300 px-2 ${columnsVisibility[1] ? columnWidthClass : "w-0 invisible"}`}
+        >
+          <ParserColumn />
+        </div>
+        <div
+          className={`transition-width duration-300 px-2 ${columnsVisibility[2] ? columnWidthClass : "w-0 invisible"}`}
+        >
+          <ParserColumn />
+        </div>
+        <div
+          className={`transition-width duration-300 pl-2 ${columnsVisibility[3] ? columnWidthClass : "w-0 invisible"}`}
         >
           <ParserColumn />
         </div>
